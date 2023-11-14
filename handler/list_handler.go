bcf294628a85b13596bb6c9cc092b022c187e78e
Jeremy Fonseca <fonseca.dev@outlook.com>
2023-11-13 18:32:35 -0600

    Add HandleGroupedListCreation and unit test it (#43).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

---
+++ handler/list_handler.go
@@ -0,0 +1,89 @@
+package handler
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/google/uuid"
+	"log"
+	"net/http"
+	"noda"
+	"noda/data/transfer"
+	"noda/service"
+)
+
+type ListHandler struct {
+	s service.ListService
+}
+
+func NewListHandler(service service.ListService) *ListHandler {
+	return &ListHandler{s: service}
+}
+
+type listType uint8
+
+const (
+	scattered listType = 1
+	grouped   listType = 2
+)
+
+func (h *ListHandler) doCreateList(t listType, w http.ResponseWriter, r *http.Request) {
+	var next = new(transfer.ListCreation)
+	var err = decodeJSONRequestBody(w, r, next)
+	if nil != err {
+		noda.EmitError(w, noda.ErrMalformedRequest.Clone().SetDetails(err.Error()))
+		return
+	}
+	err = next.Validate()
+	if nil != err {
+		noda.EmitError(w, noda.ErrBadRequest.Clone().SetDetails(err.Error()))
+		return
+	}
+	var userID, _ = extractUserPayload(r)
+	var insertedID uuid.UUID
+	if grouped == t {
+		groupID, err := parsePathParameterToUUID(r, "group_id")
+		if nil != err {
+			var e *noda.Error
+			if errors.As(err, &e) {
+				noda.EmitError(w, e)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			return
+		}
+		insertedID, err = h.s.SaveList(userID, groupID, next)
+		if nil != err {
+			var e *noda.Error
+			if errors.As(err, &e) {
+				noda.EmitError(w, e)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			return
+		}
+	} else {
+		insertedID, err = h.s.SaveList(userID, uuid.Nil, next)
+		if nil != err {
+			var e *noda.Error
+			if errors.As(err, &e) {
+				noda.EmitError(w, e)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			return
+		}
+	}
+	var result = map[string]string{"insertedID": insertedID.String()}
+	data, err := json.Marshal(result)
+	if nil != err {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(data)
+}
+
+func (h *ListHandler) HandleGroupedListCreation(w http.ResponseWriter, r *http.Request) {
+	h.doCreateList(grouped, w, r)
+}
--- handler/list_handler_test.go
+++ handler/list_handler_test.go
@@ -1,11 +1,17 @@
 package handler
 
 import (
+	"bytes"
+	"errors"
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"net/http"
+	"net/http/httptest"
 	"noda/data/model"
 	"noda/data/transfer"
 	"noda/data/types"
+	"testing"
 )
 
 type mockListService struct {
@@ -86,4 +92,133 @@ func (o *mockListService) MoveList(ownerID, listID, targetGroupID uuid.UUID) (ok
 func (o *mockListService) UpdateList(ownerID, groupID, listID uuid.UUID, up *transfer.ListUpdate) (ok bool, err error) {
 	var args = o.Called(ownerID, groupID, listID)
 	return args.Bool(0), args.Error(1)
+}
+
+func TestListHandler_HandleGroupedListCreation(t *testing.T) {
+	var groupID = uuid.New()
+	const (
+		method = "POST"
+		target = "/me/groups/{group_id}/lists"
+	)
+
+	t.Run("success", func(t *testing.T) {
+		var (
+			requestBody          = marshal(t, JSON{"name": "list name", "description": "list description"})
+			insertedID           = uuid.New()
+			expectedStatusCode   = http.StatusCreated
+			expectedResponseBody = marshal(t, JSON{"insertedID": insertedID.String()})
+			next                 = &transfer.ListCreation{Name: "list name", Description: "list description"}
+		)
+		var request = httptest.NewRequest(method, target, bytes.NewReader(requestBody))
+		withUserLoggedUser(&request)
+		withPathParameter(&request, "group_id", groupID.String())
+		var m = new(mockListService)
+		m.On("SaveList", userID, groupID, next).Return(insertedID, nil)
+		var recorder = httptest.NewRecorder()
+		NewListHandler(m).HandleGroupedListCreation(recorder, request)
+		var response = recorder.Result()
+		defer response.Body.Close()
+		var responseBody = extractResponseBody(t, response.Body)
+		assert.Equal(t, expectedStatusCode, response.StatusCode)
+		assert.Equal(t, string(expectedResponseBody), string(responseBody))
+		assert.Empty(t, response.Cookies(), "No cookie is expected, but got: %d.", len(response.Cookies()))
+		assert.Empty(t, response.Header, "No header is expected, but got: %d.", len(response.Header))
+	})
+
+	t.Run("could not decode JSON body: il-formed JSON", func(t *testing.T) {
+		var (
+			requestBody            = []byte("{")
+			expectedStatusCode     = http.StatusBadRequest
+			expectedInResponseBody = "Body contains ill-formed JSON."
+		)
+		var request = httptest.NewRequest(method, target, bytes.NewReader(requestBody))
+		var m = new(mockListService)
+		m.AssertNotCalled(t, "SaveList")
+		var recorder = httptest.NewRecorder()
+		NewListHandler(m).HandleGroupedListCreation(recorder, request)
+		var response = recorder.Result()
+		defer response.Body.Close()
+		var responseBody = extractResponseBody(t, response.Body)
+		assert.Equal(t, expectedStatusCode, response.StatusCode)
+		assert.Contains(t, string(responseBody), expectedInResponseBody)
+	})
+
+	t.Run("list creation validation failed on required fields", func(t *testing.T) {
+		var (
+			requestBody            = []byte("{}")
+			expectedStatusCode     = http.StatusBadRequest
+			expectedInResponseBody = "[\"Validation for \\\"name\\\" failed on: required.\",\"Validation for \\\"description\\\" failed on: required.\"]"
+		)
+		var request = httptest.NewRequest(method, target, bytes.NewReader(requestBody))
+		var m = new(mockListService)
+		m.AssertNotCalled(t, "SaveList")
+		var recorder = httptest.NewRecorder()
+		NewListHandler(m).HandleGroupedListCreation(recorder, request)
+		var response = recorder.Result()
+		defer response.Body.Close()
+		var responseBody = extractResponseBody(t, response.Body)
+		assert.Equal(t, expectedStatusCode, response.StatusCode)
+		assert.Contains(t, string(responseBody), expectedInResponseBody)
+	})
+
+	t.Run("parsing \"group_id\" failed: UUID is too short", func(t *testing.T) {
+		var (
+			requestBody            = marshal(t, JSON{"name": "n", "description": "d"})
+			expectedStatusCode     = http.StatusBadRequest
+			expectedInResponseBody = "Invalid UUID length."
+		)
+		var request = httptest.NewRequest(method, target, bytes.NewReader(requestBody))
+		withUserLoggedUser(&request)
+		withPathParameter(&request, "group_id", "x")
+		var m = new(mockListService)
+		m.AssertNotCalled(t, "SaveList")
+		var recorder = httptest.NewRecorder()
+		NewListHandler(m).HandleGroupedListCreation(recorder, request)
+		var response = recorder.Result()
+		defer response.Body.Close()
+		var responseBody = extractResponseBody(t, response.Body)
+		assert.Equal(t, expectedStatusCode, response.StatusCode)
+		assert.Contains(t, string(responseBody), expectedInResponseBody)
+	})
+
+	t.Run("parsing \"group_id\" failed: invalid UUID format", func(t *testing.T) {
+		var (
+			requestBody            = marshal(t, JSON{"name": "n", "description": "d"})
+			expectedStatusCode     = http.StatusBadRequest
+			expectedInResponseBody = "Invalid UUID format."
+		)
+		var request = httptest.NewRequest(method, target, bytes.NewReader(requestBody))
+		withUserLoggedUser(&request)
+		withPathParameter(&request, "group_id", "a0e2240b-8f5b-4b1e-88a9-c6d9284a6afX")
+		var m = new(mockListService)
+		m.AssertNotCalled(t, "SaveList")
+		var recorder = httptest.NewRecorder()
+		NewListHandler(m).HandleGroupedListCreation(recorder, request)
+		var response = recorder.Result()
+		defer response.Body.Close()
+		var responseBody = extractResponseBody(t, response.Body)
+		assert.Equal(t, expectedStatusCode, response.StatusCode)
+		assert.Contains(t, string(responseBody), expectedInResponseBody)
+	})
+
+	t.Run("got a service error", func(t *testing.T) {
+		var (
+			requestBody        = marshal(t, JSON{"name": "n", "description": "d"})
+			expectedStatusCode = http.StatusInternalServerError
+			unexpected         = errors.New("unexpected error")
+		)
+		var request = httptest.NewRequest(method, target, bytes.NewReader(requestBody))
+		withUserLoggedUser(&request)
+		withPathParameter(&request, "group_id", groupID.String())
+		var m = new(mockListService)
+		m.On("SaveList", mock.Anything, mock.Anything, mock.AnythingOfType("*transfer.ListCreation")).
+			Return(uuid.Nil, unexpected)
+		var recorder = httptest.NewRecorder()
+		NewListHandler(m).HandleGroupedListCreation(recorder, request)
+		var response = recorder.Result()
+		defer response.Body.Close()
+		var responseBody = extractResponseBody(t, response.Body)
+		assert.Equal(t, expectedStatusCode, response.StatusCode)
+		assert.Empty(t, string(responseBody), "No response body is expected.")
+	})
 }
