a726fdbe735a184f6f14985c5c596787bc688b88
Jeremy Fonseca <fonseca.dev@outlook.com>
2023-12-06 16:31:02 -0600

    Refactor task table (#47).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- database/tables/tasks/task_table.sql
+++ database/tables/tasks/task_table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS "task"
 (
   "task_id"          UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4 (),
-  "group_id"         UUID DEFAULT NULL REFERENCES "group" ("group_id") ON DELETE CASCADE,
   "owner_id"         UUID NOT NULL REFERENCES "user" ("user_id") ON DELETE CASCADE,
   "list_id"          UUID NOT NULL REFERENCES "list" ("list_id") ON DELETE CASCADE,
   "position_in_list" pos_t NOT NULL,
@@ -11,11 +10,9 @@ CREATE TABLE IF NOT EXISTS "task"
   "priority"         task_priority_t NOT NULL,
   "status"           task_status_t NOT NULL,
   "is_pinned"        BOOLEAN NOT NULL DEFAULT FALSE,
-  "is_archived"      BOOLEAN NOT NULL DEFAULT FALSE,
   "due_date"         TIMESTAMPTZ DEFAULT NULL,
   "remind_at"        TIMESTAMPTZ DEFAULT NULL,
   "completed_at"     TIMESTAMPTZ DEFAULT NULL,
-  "archived_at"      TIMESTAMPTZ DEFAULT NULL,
   "created_at"       TIMESTAMPTZ NOT NULL DEFAULT now (),
   "updated_at"       TIMESTAMPTZ NOT NULL DEFAULT now ()
 );
