c95ccceaff7a669ac636e8ef0b8e112436d9e65e
Jeremy Fonseca <fonseca.dev@outlook.com>
2023-12-07 12:43:25 -0600

    Adjust task fields' length.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- database/types/composites/task_creation_t.sql
+++ database/types/composites/task_creation_t.sql
@@ -2,9 +2,9 @@ DROP TYPE IF EXISTS "task_creation_t";
 
 CREATE TYPE "task_creation_t" AS
 (
-  "title"       VARCHAR(100),
-  "headline"    VARCHAR(100),
-  "description" TEXT,
+  "title"       VARCHAR(128),
+  "headline"    VARCHAR(64),
+  "description" VARCHAR(512),
   "priority"    task_priority_t,
   "status"      task_status_t,
   "due_date"    TIMESTAMPTZ,
--- database/types/composites/task_update_t.sql
+++ database/types/composites/task_update_t.sql
@@ -2,9 +2,9 @@ DROP TYPE IF EXISTS "task_update_t";
 
 CREATE TYPE "task_update_t" AS
 (
-  "title"       VARCHAR(100),
-  "headline"    VARCHAR(100),
-  "description" TEXT
+  "title"       VARCHAR(128),
+  "headline"    VARCHAR(64),
+  "description" VARCHAR(512)
 );
 
 ALTER TYPE "task_update_t"
