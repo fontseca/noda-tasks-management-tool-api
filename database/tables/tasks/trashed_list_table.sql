baf5ced8aa091cbd296a1babc6d62b622fa1c496
Jeremy Fonseca <fonseca.dev@outlook.com>
2023-12-07 13:06:32 -0600

    Remove.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- database/tables/tasks/trashed_list_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "trashed_list"
-                  AS TABLE "list";
-
-ALTER TABLE "trashed_list"
- ADD COLUMN "trashed_at" TIMESTAMPTZ NOT NULL DEFAULT now (),
- ADD COLUMN "will_destroy_at" TIMESTAMPTZ NOT NULL DEFAULT now () + INTERVAL '30d';
-
-ALTER TABLE "trashed_list"
-   OWNER TO "noda";
