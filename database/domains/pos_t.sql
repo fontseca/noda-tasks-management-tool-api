00cd1a651a1a9d38fcdc52d74d500ae81fb04a27
Jeremy Fonseca <fonseca.dev@outlook.com>
2023-12-20 16:12:54 -0600

    Change check condition of 'pos_t' domain.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- database/domains/pos_t.sql
+++ database/domains/pos_t.sql
@@ -1,8 +1,8 @@
 DROP DOMAIN IF EXISTS "pos_t";
 
 CREATE DOMAIN "pos_t"
-           AS INTEGER NOT NULL DEFAULT 0
-        CHECK (VALUE >= 0);
+           AS INTEGER NOT NULL DEFAULT 1
+        CHECK (VALUE > 0);
 
 ALTER DOMAIN "pos_t"
     OWNER TO "noda";
