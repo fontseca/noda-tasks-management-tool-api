77831e5d5228f437d927864b1bac5cf4de342c53
Jeremy Fonseca <fonseca.dev@outlook.com>
2023-12-26 12:36:46 -0600

    Add PL/pgSQL function 'move_task_to_deferred_list' (#47).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

---
+++ database/routines/functions/tasks/move_task_to_deferred_list.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION move_task_to_deferred_list
+(
+  IN p_owner_id "task"."owner_id"%TYPE,
+  IN p_task_id  "task"."task_id"%TYPE
+)
+RETURNS BOOLEAN
+RETURNS NULL ON NULL INPUT
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE
+  deferred_list_id "task"."list_id"%TYPE;
+  n_affected_rows INTEGER;
+BEGIN
+  CALL assert_user_exists (p_owner_id);
+  CALL assert_task_exists_somewhere (p_owner_id, p_task_id);
+  deferred_list_id := get_deferred_list_id (p_owner_id);
+  IF deferred_list_id IS NULL THEN
+    RETURN FALSE;
+  END IF;
+  UPDATE "task"
+     SET "list_id" = deferred_list_id
+   WHERE "task"."owner_id" = p_owner_id
+     AND "task"."task_id" = p_task_id;
+  GET DIAGNOSTICS n_affected_rows := ROW_COUNT;
+  RETURN n_affected_rows;
+END;
+$$;
+
+ALTER FUNCTION move_task_to_deferred_list ("task"."owner_id"%TYPE,
+                                           "task"."task_id"%TYPE)
+      OWNER TO "noda";
