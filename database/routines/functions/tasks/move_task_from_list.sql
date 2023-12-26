eb3e94a4035099fa07415a59d87ce8834ff7df76
Jeremy Fonseca <fonseca.dev@outlook.com>
2023-12-26 12:00:23 -0600

    Add PL/pgSQL function 'move_task_from_list' (#47).
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

---
+++ database/routines/functions/tasks/move_task_from_list.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION move_task_from_list
+(
+  IN p_owner_id "task"."owner_id"%TYPE,
+  IN p_task_id  "task"."task_id"%TYPE,
+  IN p_dst_list_id "task"."list_id"%TYPE
+)
+RETURNS BOOLEAN
+RETURNS NULL ON NULL INPUT
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE
+  n_affected_rows INTEGER;
+BEGIN
+  CALL assert_user_exists (p_owner_id);
+  CALL assert_task_exists_somewhere (p_owner_id, p_task_id);
+  CALL assert_list_exists_somewhere (p_owner_id, p_dst_list_id);
+  UPDATE "task"
+     SET "list_id" = p_dst_list_id,
+         "updated_at" = now ()
+   WHERE "task"."owner_id" = p_owner_id
+     AND "task"."task_id" = p_task_id;
+  GET DIAGNOSTICS n_affected_rows := ROW_COUNT;
+  RETURN n_affected_rows;
+END;
+$$;
+
+ALTER FUNCTION move_task_from_list ("task"."owner_id"%TYPE,
+                                    "task"."task_id"%TYPE,
+                                    "task"."list_id"%TYPE)
+      OWNER TO "noda";
