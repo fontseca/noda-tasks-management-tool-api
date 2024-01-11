2650985c99496cae366e6b18c8767de2bfbaf981
Jeremy Fonseca <fonseca.dev@outlook.com>
2024-01-11 09:28:31 -0600

    Add 'TaskHandler' struct.
    Signed-off-by: Jeremy Fonseca <fonseca.dev@outlook.com>

--- handler/task_handler.go
+++ handler/task_handler.go
@@ -1,1 +1,13 @@
 package handler
+
+import (
+	"noda/service"
+)
+
+type TaskHandler struct {
+	s service.TaskService
+}
+
+func NewTaskHandler(service service.TaskService) *TaskHandler {
+	return &TaskHandler{s: service}
+}
